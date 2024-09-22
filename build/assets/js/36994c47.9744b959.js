ba063ea78cbd8bd826137de127b92696513fb24f
Seong-Young Her <seongyher@gmail.com>
2024-09-23 06:46:50 +1200

    remove build dir from gitignore

---
+++ build/assets/js/36994c47.9744b959.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkweightliftingstudies_org=self.webpackChunkweightliftingstudies_org||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ build/assets/js/acecf23e.7bb4273e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkweightliftingstudies_org=self.webpackChunkweightliftingstudies_org||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
