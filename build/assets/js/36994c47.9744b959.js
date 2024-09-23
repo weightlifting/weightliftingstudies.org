7c73e69ae3a9a945e8c0643e5e990554af85fed6
Seong-Young Her <seongyher@gmail.com>
2024-09-23 14:52:30 +1200

    rebuild with blog disabled

--- build/assets/js/36994c47.9744b959.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweightliftingstudies_org=self.webpackChunkweightliftingstudies_org||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- build/assets/js/aba21aa0.dcd87039.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweightliftingstudies_org=self.webpackChunkweightliftingstudies_org||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
